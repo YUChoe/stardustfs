6e960ee97f25301770fdfa49d6b64b79b9a2ab28
pp@NAS5 <yonguk.choe@gmail.com>
2023-08-15 17:01:50 +0900

    Changed fn name whoami > stackname

--- stardustlib/log.py
+++ stardustlib/log.py
@@ -1,11 +1,12 @@
 import datetime
 import traceback
 
-def log(s: str) -> None:
-    print(datetime.datetime.now().strftime('%H:%M:%S.%f')[:-3], f'{who_am_i()}', s)
 
-def who_am_i():
+def log(s: str) -> None:
+    print(datetime.datetime.now().strftime('%H:%M:%S.%f')[:-3], f'{stackname()}', s)
+
+
+def stackname():
     stack = traceback.extract_stack()
-    # filename, codeline, funcName, text = stack[-2]
     filename, codeline, funcName, text = stack[-3]
     return f'{filename}\t{funcName}({codeline})'
